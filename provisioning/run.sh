343e9de8780c2316490bef81a30a6b5ffd25a955
Jonas De Taeye <jonasdt@fastmail.fm>
2014-03-29 00:39:38 -0700

    Remove local host
    Otherwise, ansible tries to provision both the vagrant and local host.
    You can use the vagrant host from within the VM.

--- provisioning/run.sh
+++ provisioning/run.sh
@@ -2,4 +2,4 @@
 apt-add-repository ppa:rquillo/ansible
 apt-get update && apt-get -y install ansible
 
-ansible-playbook -i /provisioning/hosts -l local -c local /provisioning/playbook.yml
+ansible-playbook -i /provisioning/hosts -l vagrant -c local /provisioning/playbook.yml
